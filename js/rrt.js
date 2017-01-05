09a63068f502fdffde93e59ea903ee0d2280532b
Jesse Jenks <jjenks@ups.edu>
2017-01-05 14:30:21 -0800

    Mitchell's best

--- js/rrt.js
+++ js/rrt.js
@@ -1,12 +1,14 @@
 var V,E;
+var R;
 var q_init;
 var current_num, max_num;
 var max_dist;
 var dq = 8;
-var path_check, edge_check, vert_check;
-var q_slider;
+var max_samples; // for mitchell's best
+var path_check, edge_check, vert_check, rando_check, sample_check;
+// var q_slider;
 function setup() {
-  var cv = createCanvas(700,300);
+  var cv = createCanvas(600,300);
   cv.parent('rrt');
 
   document.getElementById('path_check').innerText = 'Find Paths';
@@ -21,15 +23,30 @@ function setup() {
   vert_check = createCheckbox('', false);
   vert_check.parent('vert_check');
 
+  document.getElementById('sample_check').innerText = 'Show Sample Points';
+  sample_check = createCheckbox('', false);
+  sample_check.parent('sample_check');
+
+  // document.getElementById('rando_check').innerText = 'Toggle random/Mitchell\'s best';
+  // rando_check = createCheckbox('', false);
+  // rando_check.parent('rando_check');
+
   // document.getElementById('q_slider').innerText = 'edge lengths';
   // vert_check = createCheckbox('', false);
   // vert_check.parent('vert_check');
+
+  max_samples = 10;
 
   var q_init = {
     x: width-30,
     y: height/2,
     index: 0
   };
+
+  R = [{
+    x: Math.random()*width,
+    y: Math.random()*height
+  }];
 
   V = [q_init];
   E = [
@@ -39,19 +56,30 @@ function setup() {
     }
   ];
 
-  max_num = 500;
+  max_num = 400;
   current_num = 1;
 
   max_dist = 0;
 
   colorMode(HSB);
   noFill();
-  // background(255);
+  background(255);
 };
 
 function draw() {
   background(255);
+
   for (var i = 1; i<V.length; i++) {
+
+    if (sample_check.checked()) {
+      var c = 127 - 31*R[i].count/max_num - 31*R[i].dist/max_dist;
+      stroke(c);
+      strokeWeight(0.5);
+      line(R[i].x1, R[i].y1, R[i].x2, R[i].y2);
+      stroke(c - 15);
+      strokeWeight(5);
+      point(R[i].x1, R[i].y1);
+    }
 
     stroke(255*E[i].dist/max_dist,255,255);
     // stroke(cubehelix((E[i].dist+5)/(max_dist+10), 0, -1, 2));
@@ -93,26 +121,38 @@ function draw() {
 };
 
 function build_rrt() {
-  var q_rand = {
-    x: random(width),
-    y: random(height),
-    index: -1
-  };
+  var q_rand = mitchells_best();
+  // (rando_check.checked())? mitchells_best():{
+  //   x: random(width),
+  //   y: random(height)
+  // };
+
   var q_near = nearest_vertex(q_rand)[0];
 
   var new_dist = E[q_near.index].dist+1;
   if (new_dist > max_dist) max_dist = new_dist;
 
+  R.push({
+    x1: q_rand.x,
+    y1: q_rand.y,
+
+    x2: q_near.x,
+    y2: q_near.y,
+
+    dist: new_dist,
+    count: current_num
+  });
+
   var q_new = new_conf(q_near, q_rand);
 
   // make sure it didn't get too close to another vertex
   // if (nearest_vertex(q_new)[1] >= dq) {
-    V.push(q_new);
-    E.push({
-      dist: new_dist,
-      adj_vert: q_near
-    });
-    current_num++;
+  V.push(q_new);
+  E.push({
+    dist: new_dist,
+    adj_vert: q_near
+  });
+  current_num++;
   // }
 };
 
@@ -146,6 +186,33 @@ function distance(p,q) {
   return delta_x*delta_x + delta_y*delta_y;
 };
 
+function mitchells_best() {
+  var best_x = -1;
+  var best_y = -1;
+  var best_dist = -1;
+  for (var j = 0; j<max_samples; j++) {
+    var tx = Math.random()*width;
+    var ty = Math.random()*height;
+    var dist = Infinity;
+
+    for (var k = 0; k<current_num; k++) {
+      var dx = tx - R[k].x1;
+      var dy = ty - R[k].y1;
+      var d = dx*dx + dy*dy;
+      if (d < dist) {
+        dist = d;
+      }
+    }
+
+    if (dist > best_dist) {
+      best_x = tx;
+      best_y = ty;
+      best_dist = dist;
+    }
+  }// end j
+  return {x: best_x, y:best_y};
+}
+
 function cubehelix(lambda, s, r, hue) {
   if (lambda >= 1) return color(255);
 
@@ -160,6 +227,10 @@ function cubehelix(lambda, s, r, hue) {
 
 function mousePressed() {
   if (mouseX > -20 && mouseX < width+20 && mouseY > -20 && mouseY < height+20) {
+    R = [{
+      x: Math.random()*width,
+      y: Math.random()*height
+    }];
     V = [{
       x: mouseX,
       y: mouseY,
@@ -179,6 +250,10 @@ function mousePressed() {
 
 function keyTyped() {
   if (key === 'g') {
+    R = [{
+      x: Math.random()*width,
+      y: Math.random()*height
+    }];
     V = [{
       x: random(width),
       y: random(height),
