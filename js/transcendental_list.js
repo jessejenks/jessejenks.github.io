544cacf2aa79d8c1a7489aa9876fa80e21404083
Jesse Jenks <jjenks@ups.edu>
2017-01-05 00:15:25 -0800

    forgot a slash

--- js/transcendental_list.js
+++ js/transcendental_list.js
@@ -12,6 +12,7 @@ var transcendentals = [
     link: "http://mathworld.wolfram.com/Gelfond-SchneiderConstant.html"
   },
   {
+    // number: '\\(\\frac{1}{4}\\left(2-\\frac{1\\cdot3\\cdot15\\cdot255\\cdots}{2\\cdot4\\cdot16\\cdot256\\cdots}\\right)\\)',
     number: '\\(\\frac{1}{4}\\left(2-\\frac{1\\cdot3\\cdot15\\cdot255\\cdots}{2\\cdot4\\cdot16\\cdot256\\cdots}\\right)\\)',
     link: "http://mathworld.wolfram.com/Thue-MorseConstant.html"
   },
@@ -20,7 +21,19 @@ var transcendentals = [
     link: "http://mathworld.wolfram.com/NaturalLogarithmof2.html"
   },
   {
-    number: 'e^{\\gamma}',
+    number: '\\( e^{\\gamma} \\)',
     link: "http://mathworld.wolfram.com/MertensTheorem.html"
+  },
+  {
+    number: '\\(\\zeta(3)\\)',
+    link: "http://mathworld.wolfram.com/AperysConstant.html"
+  },
+  {
+    number: '\\(\\Gamma(\\frac{1}{2})\\)',
+    link: "http://mathworld.wolfram.com/GammaFunction.html"
+  },
+  {
+    number: '\\(\\prod_{n=1}^{\\infty}(1+\\phi^{-2n})\\)',
+    link: "http://mathworld.wolfram.com/FibonacciFactorialConstant.html"
   }
 ];
