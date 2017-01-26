9d82b71c069539f67f1df69ae604d19bc5167dbc
Jesse Jenks <jjenks@ups.edu>
2017-01-26 11:27:17 -0800

    speed up sim

--- js/lorenz.js
+++ js/lorenz.js
@@ -11,6 +11,7 @@ var buffer
 function setup() {
   buffer = 50
   var cv = createCanvas(400,400+buffer)
+  // var cv = createCanvas(400,400)
   cv.parent('lorenz')
   rho = 28
   sigma = 10
@@ -27,7 +28,7 @@ function setup() {
   maxIndex = 1000
 
   values = []
-  values[index] = {x:x, y:y, z:z}
+  values[index] = [x, y, z]
 
   // background(0)
   strokeWeight(2)
@@ -39,33 +40,35 @@ function draw() {
   background(0)
   for (var i = 0; i<index; i++) {
     // stroke(i, 255, 255);
-    stroke(i%512,255,255)
+    stroke(i%360,255,255)
     point(
-      width/2 + scale*cos(t)*values[i].x + scale*sin(t)*values[i].z,
-      height/2 - buffer/2 - scale*values[i].y
+      // width/2 + scale*cos(t)*values[i].x + scale*sin(t)*values[i].z,
+      // height/2 - buffer/2 - scale*values[i].y
+      width/2 + scale*cos(t)*values[i][0] + scale*sin(t)*values[i][2],
+      height/2 - buffer/2 - scale*values[i][1]
     )
-    stroke(0,255,255)
-    point(width*i/index, height - buffer/2 - values[i].x)
-    stroke(170,255,255)
-    point(width*i/index, height - buffer/2 - values[i].y)
-    stroke(255,255,255)
-    point(width*i/index, height - buffer/2 - values[i].z)
+    // stroke(0,255,255)
+    point(width*i/index, height - buffer/2 - (values[i][0]+values[i][1]+values[i][2])/3)
+    // stroke(170,255,255)
+    // point(width*i/index, height - buffer/2 - values[i].y)
+    // stroke(255,255,255)
+    // point(width*i/index, height - buffer/2 - values[i].z)
   }
   if (index < maxIndex) update()
   t+=dt
   if (t > TWO_PI) t-=TWO_PI
 }
 function update() {
-  dx = sigma*(y-x)
-  dy = x*(rho - z) - y
+  dx = sigma*y-sigma*x
+  dy = x*rho - x*z - y
   dz = x*y - beta*z
 
   x += dx*dt
   y += dy*dt
   z += dz*dt
 
-  index++
-  values[index] = {x:x, y:y, z:z}
+  // values[++index] = {x:x, y:y, z:z}
+  values[++index] = [x, y, z]
 
   // console.log(x+', '+y+', '+z)
 }
@@ -76,5 +79,6 @@ function mousePressed() {
   y = Math.random()*2 - 1
   z = Math.random()*2 - 1
   values = []
-  values[index] = {x:x, y:y, z:z}
+  // values[index] = {x:x, y:y, z:z}
+  values[index] = [x, y, z]
 }
