20c7b4b85155eba0c91a0d679751a2a0731fbb51
Jesse Jenks <jjenks@ups.edu>
2018-08-22 14:38:16 -0700

    a bit of optimization

--- js/color_plants.js
+++ js/color_plants.js
@@ -1,171 +1,190 @@
-var values, frontier, box_w, w, h;
-var epsilon;
-var moore;
+var values, cols, frontier, box_w, w, h, epsilon;
+var fail_counter, max_fails;
 function setup() {
 	var cv = createCanvas(windowWidth, windowHeight);
 	cv.position(0,0);
 	cv.style("z-index:-1;");
-	// cv.style("position:absolute;");
+	cv.style("position:absolute;");
+	// var cv = createCanvas(500,500);
 	var elt = document.getElementsByTagName("BODY")[0];
 	cv.parent(elt);
 
-	box_w = 24;
+	// width and height of each cell
+	box_w = 35;
 
+	// color increment
 	epsilon = 0.02;
 
+	// precompute colors
+	cols = [];
+	for (let i = 0; i<1/epsilon; ++i) {
+		cols[i] = sinebow(i*epsilon, 0.3);
+	}
+
+	max_fails = 50;
+
+	// set up arrays
 	initialize();
 
 	noStroke();
 	background(255);
-
-	moore = false;
-
-	frameRate(30);
+	frameRate(20);
 }
 function draw() {
-	let x, y;
-	let j;
-	for (let i = 0; i<frontier.length; ++i) {
-		j = frontier[i];
-		fill(sinebow(values[j], 0.3));
-		x = j%w;
-		y = floor(j/w);
+	if (frontier.length > 0) {
+		let x, y;
+		let j;
+		for (let i = 0; i<frontier.length; ++i) {
+			// frontier stores indices of cells
+			j = frontier[i];
+			// values contains the index of colors
+			fill(cols[values[j]]);
 
-		rect(x*box_w, y*box_w, box_w, box_w);
+			x = j%w;
+			y = floor(j/w);
+
+			rect(x*box_w, y*box_w, box_w, box_w);
+		}
 	}
-	if (focused) update();
+
+	// only update if window is in focus
+	// also stop updating if couldn't find an unvisited cell
+	// after 'max_fails' many attempts
+	if (focused && fail_counter<max_fails) {
+		update();
+	}
 }
+
 function update() {
 	let curr;
+	
+	let try_to_update = true;
+
 	if (frontier.length === 0) {
 		curr = floor(Math.random()*values.length);
-		values[curr] = Math.random();
+		if (values[curr] == -1) {
+			values[curr] = floor(Math.random()*cols.length);
+		} else {
+			fail_counter++;
+			try_to_update = false;
+		}
 	} else {
 		// curr = frontier.shift();
 		// curr = frontier.pop();
-		curr = (Math.random()<0.8)? frontier.shift(): frontier.pop();
+		curr = (Math.random()<0.9)? frontier.shift(): frontier.pop();
+	
+		fail_counter = 0;
 	}
 
-	// to randomize the order of checking directions
-	let a = 
-	(moore)? fisher_yates(8):fisher_yates(4);
-	// fountain
-	// [0,2,3,1];
-	let c;// = floor(Math.random()*4);
-	for (let i = 0; i<a.length; ++i) {
-		c = a[i]; 
-		// (Math.random()<0.5)?a[i]:-1;
 
-		// von Neumann neighborhood
-		if (c === 0) {
-			// north
-			if (curr-w >= 0 && values[curr-w] == -1) {
-				frontier.unshift(curr-w);
-				values[curr-w] = alter(values[curr])
-			}
-		} else if (c === 1) {
+	if (try_to_update) {
+		// to randomize the order of checking directions
+		let a = fisher_yates(4);
+		// fountain
+		// [0,2,3,1];
+		let c;
+		for (let i = 0; i<a.length; ++i) {
+			c = a[i];
 
-			// south
-			if (curr + w < values.length && values[curr+w] == -1) {
-				frontier.unshift(curr+w);
-				values[curr+w] = alter(values[curr])
-			}
-		} else if (c === 2) {
-
-			// east
-			if (curr%w < w-1 && values[curr+1] == -1) {
-				frontier.unshift(curr+1);
-				values[curr+1] = alter(values[curr])
-			}
-		} else if (c === 3) {
-
-			// west
-			if (curr%w > 0 && values[curr-1] == -1) {
-				frontier.unshift(curr-1);
-				values[curr-1] = alter(values[curr])
-			}
-		} 
-
-		// // Moore neighborhood
-		
-		if (moore) {
-			if (c === 4) {
-				// north west
-				if (curr-w-1 >= 0 && values[curr-w-1] == -1) {
-					frontier.unshift(curr-w-1);
-					values[curr-w-1] = alter(values[curr]);
+			// von Neumann neighborhood
+			// Moore neighborhood looks dumb
+			if (c === 0) {
+				// north
+				if (curr-w >= 0 && values[curr-w] === -1) {
+					frontier.unshift(curr-w);
+					values[curr-w] = alter(values[curr])
 				}
-			} else if (c === 5) {
+			} else if (c === 1) {
 
-				// north east
-				if (curr-w+1 >= 0 && values[curr-w+1] == -1) {
-					frontier.unshift(curr-w+1);
-					values[curr-w+1] = alter(values[curr]);
+				// south
+				if (curr + w < values.length && values[curr+w] == -1) {
+					frontier.unshift(curr+w);
+					values[curr+w] = alter(values[curr])
 				}
-			} else if (c === 6) {
+			} else if (c === 2) {
 
-				// south west
-				if (curr+w-1 < values.length && values[curr+w-1] == -1) {
-					frontier.unshift(curr+w-1);
-					values[curr+w-1] = alter(values[curr]);
+				// east
+				if (curr%w < w-1 && values[curr+1] == -1) {
+					frontier.unshift(curr+1);
+					values[curr+1] = alter(values[curr])
 				}
-			} else if (c === 7) {
+			} else if (c === 3) {
 
-				// south east
-				if (curr+w+1 < values.length && values[curr+w+1] == -1) {
-					frontier.unshift(curr+w+1);
-					values[curr+w+1] = alter(values[curr]);
+				// west
+				if (curr%w > 0 && values[curr-1] == -1) {
+					frontier.unshift(curr-1);
+					values[curr-1] = alter(values[curr])
 				}
 			}
-		}
-		
-
-		// counter++;
-	} // end loop
+		} // end loop
+	}
 }
 function alter (t) {
+	return (t + 1)%cols.length;
 	// return cap(t + (2*Math.random() - 1)*epsilon);
 	// let p = t + (2*Math.random() - 1)*epsilon;
 	// if (p > 1) p-=1;
 	// if (p < 0) p+=1;
 	// return p;
-	return (t + epsilon)%1;
+	// return (t + epsilon)%1;
 	// return (t + Math.random()*epsilon)%1;
 }
-function cap(t) {
-	return max(min(t,1), 0);
-}
+
+// no longer used
+// function cap(t) {
+// 	return max(min(t,1), 0);
+// }
+
+// used to randomize direction
+// TODO
+// since array size is always 4
+// (since I gave up on moore neighborhood)
+// all possible permutations could easily be stored
+// and randomly selected from
+// UPDATE
+// I did that
+// NOTE n is ignored
 function fisher_yates (n) {
-	let arr = [];
-	let i;
-	for (i = 0; i<n; ++i) arr[i] = i;
 
-	let j, temp;
-	for (i = 0; i<n-1; ++i) {
-		j = i + floor(Math.random()*(n-i));
+	return permutations[floor(Math.random()*24)];
 
-		temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
-	}
+	// let arr = [];
+	// let i;
+	// for (i = 0; i<n; ++i) arr[i] = i;
 
-	return arr;
+	// let j, temp;
+	// for (i = 0; i<n-1; ++i) {
+	// 	j = i + floor(Math.random()*(n-i));
+
+	// 	temp = arr[i];
+	// 	arr[i] = arr[j];
+	// 	arr[j] = temp;
+	// }
+
+	// return arr;
 }
+
 function initialize () {
 	w = floor(width/box_w)+1;
 	h = floor(height/box_w)+1;
 
-	values = [];
+	fail_counter = 0;
+
+	values = new Int8Array(w*h);
 	frontier = [];
 
-	for (let i = 0; i<w*h; ++i) {
+	for (let i = 0; i<values.length; ++i) {
 		values[i] = -1;
 	}
 
 	let start = 0;
-	values[start] = 0;//Math.random();
+	values[start] = 0;
 	frontier.unshift(start);
 }
+
+// u is hue
+// v is to desaturate
+// i.e. if v = 1, returns white
 function sinebow (u, v) {
 	t = 1 - (( u + 0.5) % 1);
 	r = Math.sin(PI*t);
@@ -173,8 +192,10 @@ function sinebow (u, v) {
 	b = Math.sin(PI*(t+2./3));
 	return color(255*((1-v) + v*r*r), 255*((1-v) + v*g*g), 255*((1-v) + v*b*b));
 }
+
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
+	// leaves a weird artifact without loop();
 	loop();
 	background(255);
 	initialize();
