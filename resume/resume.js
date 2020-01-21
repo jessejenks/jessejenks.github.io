1ef2e760d368903c39cac7ce00db5afe7280054f
Jesse Jenks <JesseEndoJenks@gmail.com>
2020-01-21 15:32:52 -0800

    Updates resume page styling
    Updates the resume page to use the new palette colors.
    
    Refactors how the languages table is calculated for 2 or 3 columns.
    I didn't know the liquid language very well, but now there is no code
    duplication.
    
    Refactors the code to generate the scrolling website name effect.
    
    Adds new languages and logos to the languages section.

--- resume/resume.js
+++ resume/resume.js
@@ -2,44 +2,13 @@ const animatedCodeContainer = document.querySelector('#animated-code-container')
 const animatedCode = document.querySelector('#animated-code');
 const animatedChildren = animatedCode.children;
 
-let child, styles;
+let currentPermutation = getRandomPermutation(animatedChildren.length);
 
-let scrollPosition = 0;
-let lastScrollPosition = 0;
-let ticking = false;
+const identityMatrix = "matrix(1,0,0,1,0,0)";
 
-
-const choice = a => a[Math.floor(Math.random()*a.length)];
-const permutations = [[2,1,0,3],[3,0,1,2],[3,1,0,2]];
-let currentPermutation = choice(permutations);
-
-const isVisible = elt => {
-    const { top, bottom } = elt.getBoundingClientRect();
-    return (top <= window.innerHeight) && (bottom >= 0);
-}
-
-const lowerBound = -30;
-const upperBound = 30;
-const clamp = x => {
-    if (x > upperBound) {
-        return lowerBound;
-    } else if (x < lowerBound) {
-        return upperBound;
-    } else {
-        return x;
-    }
-}
-
-const getNewMatrix = (matrix, index, scaleBy) => {
-    // gets the y translation
-    let value = parseFloat(matrix.split(',')[5]) || 0;
-    return `matrix(1,0,0,1,0,${clamp(value - scaleBy*.5*(index+1))})`;
-}
-
-const getIdentityMatrix = () => 'matrix(1,0,0,1,0,0)';
-
-const transformPositions = (scrollPosition, isScrollingUP) => {
-    const scaleBy = isScrollingUP? -1 : 1;
+function transformPositions(scrollPosition, isScrollingUP) {
+    let child, styles;
+    const direction = isScrollingUP ? -1 : 1;
     for (let i = 0; i<animatedChildren.length; i++) {
         child = animatedChildren[i];
         styles = getComputedStyle(child);
@@ -48,7 +17,7 @@ const transformPositions = (scrollPosition, isScrollingUP) => {
         const webkit   = styles.getPropertyValue('-webkit-transform');
         const ms       = styles.getPropertyValue('-ms-transform');
 
-        const args = [currentPermutation[i], scaleBy];
+        const args = [currentPermutation[i], direction];
 
         if (noPrefix && noPrefix !== 'none') {
             
@@ -64,7 +33,7 @@ const transformPositions = (scrollPosition, isScrollingUP) => {
 
         } else {
 
-            let newMatrix = getIdentityMatrix();
+            let newMatrix = identityMatrix;
             child.style['transform'] = newMatrix;
             child.style['-webkit-transform'] = newMatrix;
             child.style['-ms-transform'] = newMatrix;
@@ -73,10 +42,32 @@ const transformPositions = (scrollPosition, isScrollingUP) => {
     }
 }
 
+function getNewMatrix(matrix, index, direction) {
+    // gets the y translation
+    let value = parseFloat(matrix.split(',')[5]) || 0;
+    return `matrix(1,0,0,1,0,${clamp(value - direction * (index+1) / 4)})`;
+}
+
+const lowerBound = -30;
+const upperBound = 30;
+function clamp(x) {
+    if (x > upperBound) {
+        return lowerBound;
+    } else if (x < lowerBound) {
+        return upperBound;
+    } else {
+        return x;
+    }
+}
+
+
 let animationDidStart = false;
 
+let scrollPosition = 0;
+let lastScrollPosition = 0;
+let ticking = false;
+
 window.addEventListener('scroll', function(e) {
-    
     scrollPosition = window.scrollY || window.pageYOffset;
 
     if (!ticking && isVisible(animatedCodeContainer)) {
@@ -85,10 +76,7 @@ window.addEventListener('scroll', function(e) {
         }
         
         window.requestAnimationFrame(() => {
-            transformPositions(
-                scrollPosition,
-                scrollPosition > lastScrollPosition
-            );
+            transformPositions(scrollPosition, scrollPosition > lastScrollPosition);
             lastScrollPosition = scrollPosition;
             ticking = false;
         });
@@ -96,8 +84,31 @@ window.addEventListener('scroll', function(e) {
         ticking = true;
     } else {
         if (animationDidStart) {
-            currentPermutation = choice(permutations);
+            currentPermutation = getRandomPermutation(animatedChildren.length);
             animationDidStart = false;
         }
     }
 });
+
+function isVisible(elt) {
+    const { top, bottom } = elt.getBoundingClientRect();
+    return (top <= window.innerHeight) && (bottom >= 0);
+}
+
+function getRandomPermutation(n=0) {
+    return fisherYates([...Array(n).keys()]);
+}
+
+function fisherYates(arr=[]) {
+    const n = arr.length;
+    let j;
+    let temp;
+    for (let i = 0; i < n - 1; i++) {
+        j = Math.round(Math.random() * i);
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    return arr;
+}
