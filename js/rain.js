88930d934ca802067622638e1ac1447a8b90d941
Jesse Jenks <JesseEndoJenks@gmail.com>
2020-02-03 10:10:45 -0800

    Refactors about page and rain animation
    Refactors the about page. Adds `rel="noopener"` to all links, which is
    supposed to be a security issue.
    
    Refactors the background rain animation.

--- js/rain.js
+++ js/rain.js
@@ -1,13 +1,16 @@
-(() => {
-const cv = document.createElement('canvas');
-cv.id = 'about';
-cv.setAttribute('style', 'position: fixed; top: 0; left: 0; z-index: -1;');
-cv.width = window.innerWidth;
-cv.height = window.innerHeight;
+(function() {
+const cv = document.createElement("canvas");
+cv.setAttribute("id", "rain");
+cv.setAttribute("style", "position: fixed; top: 0; left: 0; z-index: -1; width: 100vw; height: 100vh;");
+document.querySelector("body").appendChild(cv);
 
-document.getElementsByTagName("BODY")[0].appendChild(cv);
+const pixelRatio = window.devicePixelRatio || 1;
+let rect = cv.getBoundingClientRect();
+cv.width = rect.width * pixelRatio;
+cv.height = rect.height * pixelRatio;
 
-const ctx = cv.getContext('2d');
+const ctx = cv.getContext("2d");
+ctx.scale(pixelRatio, pixelRatio);
 
 const fps = 50;
 const fpsInterval = 1000/fps;
@@ -19,26 +22,23 @@ class RainDrop {
     constructor(x, y, delay, duration) {
         this.x = x;
         this.y = y;
-        // this.radius = r;
-        this.radius = (cv.height - 2*this.y)*(23/27);
+        this.radius = (cv.height - 2 * this.y) * (23 / 27);
         this.delay = delay;
         this.animationCounter = -this.delay;
         this.duration = duration;
 
-        this.theta = 
-        // 5*Math.PI/4;
-        7*Math.PI/5;
+        this.theta = 7 * Math.PI / 5;
     }
 
     show() {
         if (this.animationCounter >= 0) {
-            let t = this.animationCounter/this.duration;
-            let rQuad = this.radius*easeOutQuad(t);
-            let rCubic = this.radius*easeOutCubic(t);
+            let t = this.animationCounter / this.duration;
+            let rQuad = this.radius * easeOutQuad(t);
+            let rCubic = this.radius * easeOutCubic(t);
 
             ctx.beginPath();
-            ctx.moveTo(this.x + rQuad*Math.cos(this.theta), this.y - rQuad*Math.sin(this.theta));
-            ctx.lineTo(this.x + rCubic*Math.cos(this.theta), this.y - rCubic*Math.sin(this.theta));
+            ctx.moveTo(this.x + rQuad * Math.cos(this.theta), this.y - rQuad * Math.sin(this.theta));
+            ctx.lineTo(this.x + rCubic * Math.cos(this.theta), this.y - rCubic * Math.sin(this.theta));
             ctx.stroke();
         }
     }
@@ -46,8 +46,9 @@ class RainDrop {
     update() {
         this.animationCounter++;
         if (this.animationCounter > this.duration) {
-            [this.x, this.y] = [randomLerp(50, cv.width+50), randomLerp(0, cv.height*.4)];
-            this.delay = randomLerp(.2,4)*fps,
+            this.x = randomLerp(50, rect.width + 50);
+            this.y = randomLerp(0, rect.height * 0.4);
+            this.delay = randomLerp(0.2, 4) * fps;
             this.animationCounter = -this.delay;
         }
     }
@@ -55,47 +56,49 @@ class RainDrop {
 
 const numRaindrops = 100;
 
-const rainAnimationDuration = 2*fps;
+const rainAnimationDuration = 2 * fps;
 
-let rainDrops;
+const rainDrops = new Array(numRaindrops);
 
-const preInitialize =() => {
-    const randomPoint = () => [randomLerp(50, cv.width+50), randomLerp(0, cv.height*.4)];
-
-    rainDrops =   [...Array(numRaindrops)]
-                    .map(_ => new RainDrop(
-                                ...randomPoint(),
-                                randomLerp(.2,4)*fps,
-                                rainAnimationDuration,
-                            )
-                        );
+function preInitialize() {
+    for (let i = 0; i < rainDrops.length; i++) {
+        rainDrops[i] = new RainDrop(
+            ...randomPoint(),
+            randomLerp(0.2, 4) * fps,
+            rainAnimationDuration,
+        );
+    }
     initialize();
 }
 
-const initialize = () => {
+const randomPoint = () => [randomLerp(50, rect.width + 50), randomLerp(0, rect.height * 0.4)];
+
+function initialize() {
+    ctx.strokeStyle = "#3e5265";
+    ctx.lineWidth = 2;
+    ctx.lineCap = "round";
+
     then = window.performance.now();
     draw();
-
-    ctx.strokeStyle = "#eef2f6";
-    // "#9bbbd8";
-    ctx.lineWidth = 3;
-    ctx.lineCap = 'round';
 }
 
 
-const draw = () => {
+function draw() {
     animationFrameID = requestAnimationFrame(draw);
 
     now = window.performance.now();
     elapsed = now - then;
 
     if (elapsed > fpsInterval) {
-        ctx.clearRect(0,0,cv.width,cv.height);
+        ctx.clearRect(0, 0, cv.width, cv.height);
 
-        rainDrops.forEach(rainDrop => {
+        let rainDrop;
+        for (let i = 0; i < rainDrops.length; i++) {
+            rainDrop = rainDrops[i];
+
             rainDrop.show();
             rainDrop.update();
-        });
+        }
 
     	then = now - (elapsed % fpsInterval);
     }
@@ -104,10 +107,10 @@ const draw = () => {
 
 window.onresize = () => {
     cancelAnimationFrame(animationFrameID);
-    cv.width = window.innerWidth;
-    cv.height = 
-    // document.getElementsByTagName("BODY")[0].scrollHeight;
-    window.innerHeight;
+    rect = cv.getBoundingClientRect();
+    cv.width = rect.width * pixelRatio;
+    cv.height = rect.height * pixelRatio;
+    ctx.scale(pixelRatio, pixelRatio);
     preInitialize();
 }
 
