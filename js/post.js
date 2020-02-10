ef9e9e4cacc30b8adaef119fced07d92f6667e16
Jesse Jenks <JesseEndoJenks@gmail.com>
2020-02-10 15:34:45 -0800

    Adds support for footnotes and references
    Adds support for css powered automatic footnote and reference numbering.
    
    Adds includes to make creating links to and from footnotes and
    references simpler.
    
    Adds event listeners to prevent adding to the history stack when
    linking to and from footnotes or references.

--- js/post.js
+++ js/post.js
@@ -1,10 +1,16 @@
-const accordions = document.querySelectorAll(".accordion");
+const footnoteLabels = document.querySelectorAll("a[aria-describedby=\"footnote-label\"]");
+const referenceLabels = document.querySelectorAll("a[aria-describedby=\"reference-label\"]");
+const backLinks = document.querySelectorAll("a[aria-label=\"Back to content\"]");
 
-for (let i = 0; i<accordions.length; i++) {
-    accordions[i].onclick = function() {
-        this.classList.toggle("active");
-        if (this.nextElementSibling) {
-            this.nextElementSibling.classList.toggle("show");
-        }
+addClickListeners(footnoteLabels);
+addClickListeners(referenceLabels);
+addClickListeners(backLinks);
+
+function addClickListeners(anchorNodes) {
+    for (let i = 0; i < anchorNodes.length; i++) {
+        anchorNodes[i].addEventListener("click", function(e) {
+            e.preventDefault();
+            window.location.replace(anchorNodes[i].href);
+        });
     }
 }
